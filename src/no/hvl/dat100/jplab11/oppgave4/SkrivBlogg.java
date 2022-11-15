ece13f2cbb6ed8adf806d0d28750aca07f7d0eff
h150697 <siri_norstrand@hotmail.com>
2022-11-15 14:00:55 +0100

    opp

--- src/no/hvl/dat100/jplab11/oppgave4/SkrivBlogg.java
+++ src/no/hvl/dat100/jplab11/oppgave4/SkrivBlogg.java
@@ -1,7 +1,11 @@
 package no.hvl.dat100.jplab11.oppgave4;
 
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
+import java.util.Scanner;
+
+import javax.swing.JOptionPane;
 
 import no.hvl.dat100.jplab11.common.TODO;
 import no.hvl.dat100.jplab11.oppgave3.*;
@@ -9,7 +13,21 @@ import no.hvl.dat100.jplab11.oppgave3.*;
 public class SkrivBlogg {
 
 	public static boolean skriv(Blogg samling, String mappe, String filnavn) {
-
-		throw new UnsupportedOperationException(TODO.method());
+		PrintWriter skriver = null;
+		boolean ok = true; 
+		
+		do {
+		try {
+			skriver = new PrintWriter(mappe + filnavn);
+			skriver.println(samling.toString());
+			skriver.close();
+		}
+		
+		catch(FileNotFoundException e) {
+			System.out.println("Katalogen finnes ikke.");
+			ok = false; 
+		}
+	}	while (!ok);
+		return ok;
 	}
 }
